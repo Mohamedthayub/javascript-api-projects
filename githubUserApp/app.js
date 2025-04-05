efd19875c48b5a6c713ecf9a1f6da01e53f63920
buyath <buyaththayub@gmail.com>
2025-04-05 23:16:08 +0000

    working on githubuserprofile

--- githubUserApp/app.js
+++ githubUserApp/app.js
@@ -1,18 +1,33 @@
-const URL = "https://api.github.com/users/Mohamedthayub";
+let button = document.getElementById("btn");
+let container  = document.getElementById("cont");
+let userName = document.querySelector("#username");
+let userImage = document.querySelector(".user-image");
+let userId = document.querySelector("#userid");
+let userBio = document.querySelector("userbio");
+const URL = "https://api.github.com/users/torvalds";
 
 async function getData(){
     try{
         const data = await fetch(URL);
-    
         const response = await data.json();
-        
-        console.log(response)
+        userImage.src = response.avatar_url;
+        userName.innerText = response.name;
+        userId.innerText = "@" + response.login;
+        userBio.innerText = response.bio;
 
+        
     }
     catch(err){
         console.log(err);
     }
-    
 }
+button.addEventListener("click",() => {
+    container.classList.add("profile-container");
+    container.removeAttribute("id");
+    getData();
 
-getData();
+})
+// getData();
+
+
+
