91f3a88a8299cb5705041032858420d42fbf0321
buyath <buyaththayub@gmail.com>
2025-04-07 00:55:43 +0000

    add username validation to GitHub user search

--- githubUserApp/app.js
+++ githubUserApp/app.js
@@ -1,33 +1,59 @@
-let button = document.getElementById("btn");
-let container  = document.getElementById("cont");
-let userName = document.querySelector("#username");
-let userImage = document.querySelector(".user-image");
-let userId = document.querySelector("#userid");
-let userBio = document.querySelector("userbio");
-const URL = "https://api.github.com/users/torvalds";
+let userName = document.getElementById("userinput").value;
+function  validateUser(username){
+  const validPattern = /^[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*$/;
+  if(!username){
+    alert("username must be valid");
+  }
+  if(userName.length > 39){
+    alert("username should not exceed 39 character");
+  }
+  if (!validPattern.test(userName)){
+    alert("Username can only contain letters, numbers, and hyphens (no spaces or special characters")
+  }
+  return username;
+  
+}
 
-async function getData(){
+async function getUser(){
+  if(userName.value == ""){
+    alert("it should not be empty");
+  }
+  else{
+    let URL = `https://api.github.com/users/${userName}`; 
     try{
-        const data = await fetch(URL);
-        const response = await data.json();
-        userImage.src = response.avatar_url;
-        userName.innerText = response.name;
-        userId.innerText = "@" + response.login;
-        userBio.innerText = response.bio;
-
+      const response = await fetch(URL);
+      if(!response.ok){
+        switch(response.status){
+          case 400:
+            throw new Error("Bad Request");
+          case 401:
+            throw new Error("Unauthorized,Please check your credentials");
+          
+          case 404:
+            throw new Error("User Not Found");
+  
+          case 500:
+            throw new Error("Server Error Please try again later");
+  
+          default:
+            throw new Error("Unexpected Erro" + response.statusText);
+        }
         
+      }
+      const data = await response.json();
+      createUser(data);
     }
     catch(err){
-        console.log(err);
+      console.log(err.message);
     }
+  }
 }
-button.addEventListener("click",() => {
-    container.classList.add("profile-container");
-    container.removeAttribute("id");
-    getData();
 
-})
-// getData();
+const seachBtn = document.querySelector("#search-btn");
 
-
-
+function createUser(data){
+  console.log(data);
+}
+seachBtn.addEventListener("click",function(e){
+  e.preventDefault();
+});
